97d05b084527737678dfcf3c07acaf2827736efd
3tipo <3tipo.quadrado.lda@gmail.com>
2021-07-30 12:27:09 +0100

    version 2

--- an/src/main/java/org/academia/an/AnApplication.java
+++ an/src/main/java/org/academia/an/AnApplication.java
@@ -2,12 +2,19 @@ package org.academia.an;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AnApplication {
+public class AnApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AnApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AnApplication.class, args);
 	}
 
+	
 }
